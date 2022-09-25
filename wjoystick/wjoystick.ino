70a7e0873fc1e2fa80a9495082e6cb5bd93b0143
amadeok <43666702+amadeok@users.noreply.github.com>
2022-09-25 15:51:06 +0200

    update

---
+++ wjoystick/wjoystick.ino
@@ -0,0 +1,294 @@
+
+#include "esp_heap_caps.h"
+#include "esp_log.h"
+#include "esp_timer.h"
+#include "esp_types.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "sdkconfig.h"
+#include <stdio.h>
+#include <string.h>
+#include "esp_system.h"
+#include "esp_wifi.h"
+#include "esp_event.h"
+#include "esp_netif.h"
+
+#include "lwip/err.h"
+#include "lwip/sockets.h"
+#include "lwip/sys.h"
+#include <lwip/netdb.h>
+
+#include "freertos/event_groups.h"
+#include "esp_event_loop.h"
+#include "nvs_flash.h"
+#include <driver/adc.h>
+
+#define WIFI_SSID "your_wifi_ssid"
+#define WIFI_PASS "your_wifi_password"
+
+#define PORT 3333
+int buf_size;
+int sock = 0;
+
+// Event group
+static EventGroupHandle_t wifi_event_group;
+const int CONNECTED_BIT = BIT0;
+static const char *TAG = "wireless_joystick";
+
+void loop(){return;}
+// Wifi event handler
+static esp_err_t event_handler(void *ctx, system_event_t *event)
+{
+  switch (event->event_id)
+  {
+
+  case SYSTEM_EVENT_STA_START:
+    esp_wifi_connect();
+    break;
+
+  case SYSTEM_EVENT_STA_GOT_IP:
+    xEventGroupSetBits(wifi_event_group, CONNECTED_BIT);
+    break;
+
+  case SYSTEM_EVENT_STA_DISCONNECTED:
+  {
+    xEventGroupClearBits(wifi_event_group, CONNECTED_BIT);
+    ESP_LOGI(TAG, "Wi-Fi disconnected, trying to reconnect...");
+    esp_err_t err = esp_wifi_connect();
+    if (err == ESP_ERR_WIFI_NOT_STARTED)
+    {
+      return err;
+    }
+    ESP_ERROR_CHECK(err);
+    break;
+  }
+  default:
+   {
+    break;
+    }
+  }
+
+  return ESP_OK;
+}
+
+// Main task
+tcpip_adapter_ip_info_t wifi_task(void *pvParameter)
+{
+  if (heap_caps_check_integrity_all(true) == 1)
+    ESP_LOGI(TAG, "Checking heap integrity: OK ");
+  else
+    ESP_LOGI(TAG, "Heap is corrupted");
+  // wait for connection
+  printf("Main task: waiting for connection to the wifi network... \n");
+  xEventGroupWaitBits(wifi_event_group, CONNECTED_BIT, false, true, 50000000);
+  printf("connected!\n");
+
+  // print the local IP address
+  tcpip_adapter_ip_info_t ip_info;
+  ESP_ERROR_CHECK(tcpip_adapter_get_ip_info(TCPIP_ADAPTER_IF_STA, &ip_info));
+  printf("IP Address:  %s\n", ip4addr_ntoa(&ip_info.ip));
+  printf("Subnet mask: %s\n", ip4addr_ntoa(&ip_info.netmask));
+  printf("Gateway:     %s\n", ip4addr_ntoa(&ip_info.gw));
+  return ip_info;
+  // while (1)
+  //  vTaskDelay(4000 / portTICK_PERIOD_MS);
+}
+
+static void cleanup( int listen_sock )
+{
+  ESP_LOGI(TAG, "Restarting the board in 300ms..");
+  close(listen_sock);
+  vTaskDelay(300 / portTICK_PERIOD_MS);
+  esp_restart();
+}
+
+static void tcp_server_task(void *pvParameter)
+{
+  char addr_str[128];
+  int addr_family;
+  int ip_protocol;
+  ESP_LOGI(TAG, "tcp_server_task");
+
+#ifdef CONFIG_EXAMPLE_IPV4
+  struct sockaddr_in dest_addr;
+  dest_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+  dest_addr.sin_family = AF_INET;
+  dest_addr.sin_port = htons(PORT);
+  addr_family = AF_INET;
+  ip_protocol = IPPROTO_IP;
+  inet_ntoa_r(dest_addr.sin_addr, addr_str, sizeof(addr_str) - 1);
+#else // IPV6
+  struct sockaddr_in6 dest_addr;
+  bzero(&dest_addr.sin6_addr.un, sizeof(dest_addr.sin6_addr.un));
+  dest_addr.sin6_family = AF_INET6;
+  dest_addr.sin6_port = htons(PORT);
+  addr_family = AF_INET6;
+  ip_protocol = IPPROTO_IPV6;
+  inet6_ntoa_r(dest_addr.sin6_addr, addr_str, sizeof(addr_str) - 1);
+#endif
+
+  int listen_sock = socket(addr_family, SOCK_STREAM, ip_protocol);
+  int yes = 0;
+  int result = setsockopt(listen_sock,
+                          IPPROTO_TCP,
+                          TCP_NODELAY,
+                          (char *)&yes,
+                          sizeof(int)); // 1 - on, 0 - off
+  if (result < 0)
+    printf("error setting tcp socket options\n");
+  if (listen_sock < 0)
+  {
+    ESP_LOGE(TAG, "Unable to create socket: errno %d", errno);
+    //goto CLEAN_UP; 
+    cleanup(listen_sock);
+    vTaskDelete(NULL);
+    return;
+  }
+  ESP_LOGI(TAG, "Socket created");
+
+  int err = bind(listen_sock, (struct sockaddr *)&dest_addr, sizeof(dest_addr));
+  if (err != 0)
+  {
+    ESP_LOGE(TAG, "Socket unable to bind: errno %d", errno);
+    cleanup(listen_sock);
+//    goto CLEAN_UP;
+  }
+  ESP_LOGI(TAG, "Socket bound, port %d", PORT);
+
+  err = listen(listen_sock, 1);
+  if (err != 0)
+  {
+    ESP_LOGE(TAG, "Error occurred during listen: errno %d", errno);
+    cleanup(listen_sock);
+//    goto CLEAN_UP;
+  }
+  while (1)
+  {
+
+    ESP_LOGI(TAG, "Socket listening");
+    struct sockaddr_in6 source_addr; // Large enough for both IPv4 or IPv6
+    uint addr_len = sizeof(source_addr);
+    sock = accept(listen_sock, (struct sockaddr *)&source_addr, &addr_len);
+    result = setsockopt(sock,
+                        IPPROTO_TCP,
+                        TCP_NODELAY,
+                        (char *)&yes,
+                        sizeof(int)); // 1 - on, 0 - off
+    if (result < 0)
+      printf("error setting tcp socket options\n");
+    if (sock < 0)
+    {
+      ESP_LOGE(TAG, "Unable to accept connection: errno %d", errno);
+    cleanup(listen_sock);
+//    goto CLEAN_UP;
+      break;
+    }
+
+    // Convert ip address to string
+    if (source_addr.sin6_family == PF_INET)
+    {
+      inet_ntoa_r(((struct sockaddr_in *)&source_addr)->sin_addr.s_addr, addr_str, sizeof(addr_str) - 1);
+    }
+    else if (source_addr.sin6_family == PF_INET6)
+    {
+      inet6_ntoa_r(source_addr.sin6_addr, addr_str, sizeof(addr_str) - 1);
+    }
+    ESP_LOGI(TAG, "Socket accepted ip address: %s", addr_str);
+    //  if (already_got_settings == false)
+    int32_t n = 0;
+    uint8_t ack[8] = {0, 0, 0, 0, 0, 0, 0, 0};
+    int32_t xpot, ypot;
+    int ret;
+
+    while (1)
+    {
+      xpot = adc1_get_raw(ADC1_CHANNEL_0);
+      ypot = adc1_get_raw(ADC1_CHANNEL_3);
+
+      // ESP_LOGI(TAG, "pot val: %d %d", xpot, ypot);
+
+      memcpy(ack, &xpot, 4);
+      memcpy(ack + 4, &ypot, 4);
+
+       ret = send(sock, ack, 8, 0);
+      //ret = recv(sock, ack, 8, 0);
+      vTaskDelay(10 / portTICK_PERIOD_MS);
+
+      if (ret == -1){
+          cleanup(listen_sock);
+        }
+        //goto CLEAN_UP;
+    }
+
+    // dma_buffer = epd_get_current_buffer();
+  }
+
+// CLEAN_UP:
+//   ESP_LOGI(TAG, "Restarting the board in 300ms..");
+//   close(listen_sock);
+//   vTaskDelay(300 / portTICK_PERIOD_MS);
+//   esp_restart();
+  // vTaskDelete(NULL);
+  // wifi_task(NULL);
+  //  xTaskCreatePinnedToCore(&tcp_server_task, "tcp_server_task", 10000, NULL, 5, NULL, 1);
+}
+
+void setup()
+{
+  // memset(clear, 0, 2);
+
+  adc1_config_width(ADC_WIDTH_BIT_12);
+  adc1_config_channel_atten(ADC1_CHANNEL_0, ADC_ATTEN_DB_11);
+  adc1_config_channel_atten(ADC1_CHANNEL_3, ADC_ATTEN_DB_11);
+
+  esp_log_level_set("wifi", ESP_LOG_NONE);
+
+  heap_caps_print_heap_info(MALLOC_CAP_INTERNAL);
+  heap_caps_print_heap_info(MALLOC_CAP_SPIRAM);
+  // initialize NVS
+  ESP_ERROR_CHECK(nvs_flash_init());
+
+  // create the event group to handle wifi events
+  wifi_event_group = xEventGroupCreate();
+
+  // initialize the tcp stack
+  tcpip_adapter_init();
+
+  if (heap_caps_check_integrity_all(true) == 1)
+    ESP_LOGI(TAG, "Checking heap integrity: OK ");
+  else
+    ESP_LOGI(TAG, "Heap is corrupted");
+
+  // initialize the wifi event handler
+  ESP_ERROR_CHECK(esp_event_loop_init(event_handler, NULL));
+
+  // initialize the wifi stack in STAtion mode with config in RAM
+  wifi_init_config_t wifi_init_config = WIFI_INIT_CONFIG_DEFAULT();
+
+  ESP_ERROR_CHECK(esp_wifi_init(&wifi_init_config));
+
+  ESP_ERROR_CHECK(esp_wifi_set_storage(WIFI_STORAGE_RAM));
+
+  ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA));
+
+  // configure the wifi connection and start the interface
+   wifi_config_t wifi_config = {
+      .sta = {
+           .ssid = {0},
+         .password = {0},
+       },
+   };
+   // wifi_config_t wifi_config;
+    strncpy( (char*)wifi_config.sta.ssid,WIFI_SSID,32);
+    strncpy( (char*)wifi_config.sta.password,WIFI_PASS,64);
+
+
+  ESP_ERROR_CHECK(esp_wifi_set_config(WIFI_IF_STA, &wifi_config));  //ESP_IF_WIFI_STA
+  ESP_ERROR_CHECK(esp_wifi_set_ps(WIFI_PS_NONE));
+
+  ESP_ERROR_CHECK(esp_wifi_start());
+
+  wifi_task(NULL);
+
+  xTaskCreatePinnedToCore(&tcp_server_task, "tcp_server_task", 10000, NULL, 5, NULL, 1);
+}
